0637c7f13437d92e2844d3ceab2f12e351fcd1cc
habitatchen <habitatchen@163.com>
2018-09-21 22:05:25 +0800

    rebuild

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/',
     proxyTable: {
       '/travel-mobile/dist/api':{
         target:'http://localhost:8080',
